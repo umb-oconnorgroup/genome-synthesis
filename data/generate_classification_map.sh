47f8a54190f870f4eee0f1d5c41644f146bd1951
Roland Laboulaye <rlaboulaye@gmail.com>
2020-10-06 07:46:47 -0400

    useful data scripts

--- data/generate_classification_map.sh
+++ data/generate_classification_map.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# kgp_samples.csv or similar file should be first argument
+# Example Usage: ./generate_classification_map.sh kgp_samples.csv
+DATA_FILE=$1
 OUTPUT_FILE=classification_map.tsv
 while read p; do
     echo "$p" | grep -oE "^[A-Za-z0-9]+,[A-Za-z0-9]+" | tr "," "\t" >> $OUTPUT_FILE
-done < "$1"
-sed -i '1d' "$1"
+done < $DATA_FILE
+sed -i '1d' $OUTPUT_FILE
---
+++ data/isolate_population.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Example Usage: ./isolate_population.sh chr20_kgp_abridged.vcf kgp_samples.csv CEU
+VCF_FILE=$1
+POPULATION_FILE=$2
+POPULATION=$3
+FILE_NAME=$(basename $VCF_FILE)
+OUTPUT_VCF_FILE="$POPULATION.${FILE_NAME%.gz}"
+POPULATION_SAMPLES=""
+SAMPLES=$(bcftools query -l $VCF_FILE)
+while read p; do
+    SAMPLE=$(echo "$p" | grep -oE "^[A-Za-z0-9]+,$POPULATION" | grep -oE "^[A-Za-z0-9]+")
+    if ! [ -z "$SAMPLE" ] && [[ "$SAMPLES" == *"$SAMPLE"* ]]; then
+        POPULATION_SAMPLES+="$SAMPLE,"
+    fi
+done < $POPULATION_FILE
+POPULATION_SAMPLES=${POPULATION_SAMPLES%,}
+bcftools view -s $POPULATION_SAMPLES $VCF_FILE > $OUTPUT_VCF_FILE
